7f62e42c60c09157e41bf79063ad20c4225ee0b0
Shakib-ewu <hossainsakib58@gmail.com>
2024-05-20 20:09:15 +0600

    new test case added

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
 
     baseUrl: "https://rubinoshoes.com/"
     
   },
+
+  // cypress/plugins/index.js
+  
 });
--- cypress/e2e/Dashboard.cy.js
+++ cypress/e2e/Dashboard.cy.js
@@ -15,10 +15,9 @@ describe("Verifying Homepage", () => {
         //cy.get("a[class='active']").click() logo click
 
     })
-    it.skip("Verifying Hero Banner", () => {
+    it("Verifying Hero Banner", () => {
        
-        cy.wait(4000)
-        cy.get("button[class='p-0 m-0 block md:inline-flex w-full']").contains('accept').click()
+        cy.wait(6000)
         cy.get('.swiper-slide-active > .relative > .absolute').click()
         cy.get('.h-full > .text-center').contains('All Products')
         cy.wait(5000)
@@ -38,7 +37,7 @@ describe("Verifying Homepage", () => {
 
     })
 
-    it.skip("Brand Scroller verification" , ()=>{
+    it("Brand Scroller verification" , ()=>{
         cy.get('.brand-container > :nth-child(1) > .swiper').should('be.visible')
         cy.get('.swiper-slide-active > .w-full').eq(1).click({force:true})
         cy.get('.h-full > .text-center').contains('Crocs')
@@ -69,7 +68,7 @@ describe("Verifying Homepage", () => {
 
     })
 
-    it("Product Scroller section verification" , ()=>{
+    it("Highlited collection section verification" , ()=>{
 
         cy.scrollTo('bottom')
         cy.get('.py-4> :nth-child(1)').eq(1).click({force:true})
@@ -85,7 +84,95 @@ describe("Verifying Homepage", () => {
         cy.wait(5000)
         cy.scrollTo('bottom')
         cy.get('.py-4> :nth-child(1)').eq(3).click({force:true})
-        cy.get('.bg-#000')
+        cy.get('h3').contains('SIZE GUIDE')
+        cy.get("body > header:nth-child(2) > nav:nth-child(1) > div:nth-child(1) > a:nth-child(1)").click()
+
+        cy.wait(5000)
+        cy.scrollTo('bottom')
+        cy.get('.py-4> :nth-child(1)').eq(0).click({force:true})
+        cy.get('.faq-banner-heading').should('have.text','frequently asked questions')
         cy.get("body > header:nth-child(2) > nav:nth-child(1) > div:nth-child(1) > a:nth-child(1)").click()
 })
+
+it("Instagram section verification", () => {
+  cy.scrollTo("bottom");
+  cy.get(
+    '[style="min-height: 100px; position: relative;"] > .container > .text-black'
+  ).should("have.text", "FOLLOW US ON INSTAGRAM");
+  //cy.get('.text-blue').click({force:true})
+
+  cy.get("div[id='KHWHNUVMmnqbaVF2EHOLc'] button:nth-child(1)")
+    .should("be.visible")
+    .click({ force: true });
+  cy.get(".p-0 > .text-black").should("have.text", "SHOP THIS LOOK");
+  cy.get(".absolute > .bg-black").click();
+  //cy.get('[data-carousel-next="true"]').click()
+  //cy.get('[data-carousel-next="true"]').click()
+  // cy.get('[data-carousel-next="true"]').click()
+  for (let i = 0; i < 4; i++) {
+    cy.get('[data-carousel-next="true"]').click();
+  }
+  cy.log("All slider completed");
+
+  for (let i = 0; i < 5; i++) {
+    cy.get('[data-carousel-prev="true"]').click();
+  }
+  cy.log("All slider completed");
+
+  cy.get("a[class='w-full h-full flex rb-product-thumb']").click({force:true}) //a[class='w-full h-full flex rb-product-thumb']
+
+
+});
+
+it('Verifying static feature section & Membership ATC',()=>{
+    cy.scrollTo("bottom");
+    cy.get('.container > :nth-child(1) > .text-center').should('have.text','BECOME A RUBINO MEMBER')
+
+    cy.get("button[type='submit']").contains(' ADD TO CART').click({force:true})
+    cy.wait(4000)
+    cy.log("ATC visible")
+    cy.get('.cart-line-quantity > .flex-row > :nth-child(3) > .cursor-pointer').click({force:true})
+    cy.get("button[type='submit']").eq(0).click({force:true})
+    cy.get('h3').should('have.text','Your cart is empty')
+    cy.get("button[class='w-full h-full items-center cursor-pointer hidden lg:block justify-end']").click()
+})
+it('Verifying footer sections',()=>{
+    cy.scrollTo("bottom");
+    cy.get('[href="/privacy-policy/"]').click({force:true})
+    cy.wait(4000)
+    cy.get("body > header:nth-child(2) > nav:nth-child(1) > div:nth-child(1) > a:nth-child(1)").click()
+    cy.get('[href="/terms-conditions/"]').click({force:true})
+    cy.wait(4000)
+
+    // FAQ, ONLINE RETURNS, SIZE GUIDE
+    cy.get("body > header:nth-child(2) > nav:nth-child(1) > div:nth-child(1) > a:nth-child(1)").click()
+    cy.get(':nth-child(2) > .text-zinc-800').click({force:true})
+    cy.wait(4000)
+    cy.get("body > header:nth-child(2) > nav:nth-child(1) > div:nth-child(1) > a:nth-child(1)").click()
+    cy.get(':nth-child(3) > .text-zinc-800').click({force:true})
+    cy.wait(4000)
+    cy.get('.p-0 > .py-4').click()
+    cy.get(':nth-child(4) > .text-zinc-800').click({force:true})
+    cy.wait(4000)
+    cy.get("body > header:nth-child(2) > nav:nth-child(1) > div:nth-child(1) > a:nth-child(1)").click()
+
+    //about,store,career,contact
+
+    cy.get('.grid> :nth-child(1) > .text-black').click({force:true})
+    cy.get('.grid> :nth-child(2) > .text-black').click({force:true})
+    cy.get('.grid> :nth-child(3) > .text-black').click({force:true})
+    cy.get('.grid > :nth-child(4)> .text-black').click({force:true})
+
+    cy.on("window:confirm",(t)=>{
+       expect(t).to.contains('Know your location')
+    })
+    cy.get("body > header:nth-child(2) > nav:nth-child(1) > div:nth-child(1) > a:nth-child(1)").click()
+    
+
+   
+
+
+})
+
+
 })
