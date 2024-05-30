7215df1a09767525cfa103d15da9a4d8fdd15289
Shakib-ewu <hossainsakib58@gmail.com>
2024-05-30 21:26:13 +0600

    new test case added

--- cypress/e2e/Account.cy.js
+++ cypress/e2e/Account.cy.js
@@ -34,50 +34,6 @@ Cypress.on('uncaught:exception', (err) => {
   }
 })
 
-// cypress/integration/your_test_spec.js
-
-/*it.only('Verify User Can Log In', () => {
-  cy.log('Clicking on account icon');
-  cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click();
-
-  cy.log('Clicking on login link');
-  cy.xpath("(//a[normalize-space()='Log in'])[1]").click();
-
-  cy.log('Filling in login form');
-  cy.get("input[placeholder='Email Address']").type('ve4dfffbgfg09897ggsot82fdf226@sparkroi.com');
-  cy.get("input[placeholder='Password']").type('12345678');
-  cy.get("button[type='submit']").click();
-
-  cy.log('Checking for error message');
-  cy.contains('Incorrect email or password').then($error => {
-    if ($error.length > 0) {
-      cy.log('Error message found: Incorrect email or password');
-      cy.contains('Incorrect email or password').should('exist');
-
-      cy.log('Navigating to sign up page');
-      cy.xpath("(//a[normalize-space()='Sign up'])[1]").click();
-
-      cy.log('Verifying sign up page URL');
-      cy.url().should('include', '/account/register/');
-
-      cy.log('Filling in sign up form');
-      cy.get("input[placeholder='First name']").type('Abn');
-      cy.get("input[placeholder='Last name']").type('Mahmud');
-      cy.get("input[placeholder='Email address']").type('ve4dfffbgfg09897ggsot82fdf226@sparkroi.com');
-      cy.get("input[placeholder='Password']").type('12345678');
-      cy.get("button[type='submit']").click();
-
-      cy.log('Verifying welcome message');
-      cy.get('h3').contains('Welcome To RUBINO').should('exist');
-    } else {
-      cy.log('Login was successful');
-      cy.url().should('not.include', '/login'); // Verify that the URL has changed from the login page
-      cy.get('h1').should('contain', 'Welcome')
-      // Add any additional checks for the logged-in state
-    }
-  });
-});*/
-
 
 it('Verify User Can Log In', () => {
   cy.log('Clicking on account icon');
@@ -124,7 +80,7 @@ it('Verify User Can Log In', () => {
   });
 });
 
-it('Verify Account Information Content', () => {
+it.skip('Verify Account Information Content', () => {
   cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click();
   cy.log('Clicking on login link');
   cy.xpath("(//a[normalize-space()='Log in'])[1]").click();
@@ -143,7 +99,7 @@ it('Verify Account Information Content', () => {
 
 })
 
-it('Verify Personal Information Page Tests', () => {
+it.skip('Verify Personal Information Page Tests', () => {
   cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click();
   cy.log('Clicking on login link');
   cy.xpath("(//a[normalize-space()='Log in'])[1]").click();
@@ -197,7 +153,8 @@ it('Verify Personal Information Page Tests', () => {
     cy.get("button[type='submit']").click();
     cy.log('Checking content of edit password page');
     cy.wait(3000)
-    cy.get('[href="/account/password/"]').click()
+    cy.get('[href="/account/password/"]').click({force:true})
+    cy.wait(4000)
     cy.get("#currentPassword").type(12345678).should('be.visible').and('have.attr', 'placeholder', 'Current password')
     cy.get("#newPassword").type(123456789).should('be.visible').and('have.attr', 'placeholder', 'New password')
     cy.get("#newPasswordConfirm").type(123456789).should('be.visible').and('have.attr', 'placeholder', 'Confirm new password')
@@ -210,7 +167,7 @@ it('Verify Personal Information Page Tests', () => {
 
   })
 
-  it.only('Verify Order History', () => {
+  it('Verify Order History', () => {
     cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click();
     cy.log('Clicking on login link');
     cy.xpath("(//a[normalize-space()='Log in'])[1]").click();
@@ -229,5 +186,174 @@ it('Verify Personal Information Page Tests', () => {
 
 
   })
+
+  it('Verify Address Book', () => {
+    cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click();
+    cy.log('Clicking on login link');
+    cy.xpath("(//a[normalize-space()='Log in'])[1]").click();
+    cy.log('Filling in login form');
+    cy.get("input[placeholder='Email Address']").type('ve4df6765897ggsot82fdf226@sparkroi.com');
+    cy.get("input[placeholder='Password']").type('123456789');
+    cy.get("button[type='submit']").click();
+    cy.log('Checking Address book creation');
+    cy.wait(3000);
+
+    cy.get('[href="/account/addresses/"]').click();  // Click on address block
+
+    // Check if an address already exists
+    cy.get('body').then(($body) => {
+        if ($body.find('.p-4 > .flex:contains("You haven’t added any address yet")').length) {
+            // No address found, proceed to add new address
+            cy.log('No address found, adding new address');
+            cy.get('.existing-address > .inline-flex').contains('add new address').click();
+            cy.get("#addressName").type('Abn').should('be.visible');
+            cy.get("#company").type("Bevy Commerce").should('be.visible');
+            cy.get("#address1").type('4242 Griffin Street').should('be.visible');
+            cy.get("#address2").type('NY').should('be.visible');
+            cy.get("#country").select('United States').should('have.value', 'United States');
+            cy.get('#province').select('Iowa').should('have.value', 'Iowa');
+            cy.get('#zip').type(50320).should('be.visible');
+            cy.get('#phone').type(4433685962).should('be.visible');
+            cy.xpath("(//*[name()='svg'][@class='absolute'])[1]").click({ force: true });
+            cy.xpath("//button[normalize-space()='Save changes']").contains('Save changes').click();
+            cy.xpath("(//small[@class='text-[#559937]'])[1]").should('have.text', 'Added New Address Successfully');
+        } else {
+            // Address exists, handle accordingly
+            cy.log('Address already exists');
+            // You can add logic here to update or verify the existing address
+        }
+    });
+});
+
+
+it('Verify Order Returns', () => {
+  cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click();
+  cy.log('Clicking on login link');
+  cy.xpath("(//a[normalize-space()='Log in'])[1]").click();
+  cy.log('Filling in login form');
+  cy.get("input[placeholder='Email Address']").type('ve4df6765897ggsot82fdf226@sparkroi.com');
+  cy.get("input[placeholder='Password']").type('123456789');
+  cy.get("button[type='submit']").click();
+  cy.log('Checking content of Personal Information Page');
+  cy.wait(3000)
+
+  cy.get('[href="/account/order-returns/"]').contains('order Returns').click({force:true})  //clicking order history page
+  cy.xpath("(//a[normalize-space()='Start now'])[1]").click({force:true})
+  cy.get("input[placeholder='Order Number']").type(1071)
+  cy.get("input[placeholder='Shipping Zip Code or Email']").type('sakib.sarkar@bevycommerce.com')
+  cy.get("button[type='submit']").contains('Start A Return').click()
+  cy.get('.py-2 > .m-0').should('have.text','Incorrect Order Number | Zip Code | Email').should('be.visible')
+  cy.get("button[type='button']").click()
+
+})
+
+
+it('Verify Rubino Membership', () => {
+  cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click();
+  cy.log('Clicking on login link');
+  cy.xpath("(//a[normalize-space()='Log in'])[1]").click();
+  cy.log('Filling in login form');
+  cy.get("input[placeholder='Email Address']").type('ve4df6765897ggsot82fdf226@sparkroi.com');
+  cy.get("input[placeholder='Password']").type('123456789');
+  cy.get("button[type='submit']").click();
+  cy.log('Checking Rubino Membership');
+  cy.wait(3000)
+
+  cy.get('[href="/account/membership/"]').click({force:true})  //clicking rubino membership
+  cy.get('.max-w-full > .inline-flex').click()
+  cy.wait(5000)
+
+  cy.get("button[class='w-full h-full items-center cursor-pointer hidden lg:block justify-end']").click()
+  cy.get('#mainContent > .flex-row > .flex > .text-black').should('have.text','You’re A Rubino Member.')
+
+  cy.xpath("(//a[normalize-space()='Browse Member Zone'])[1]").click({force:true})
+  cy.get('h3').should('have.text','ALL DEPARTMENTS')
+  cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click({force:true});
+  cy.get('[href="/account/membership/"]').click({force:true})
+  cy.xpath("(//a[normalize-space()='go to shop'])[1]").as('btn').click({force:true})
+  cy.wait(4000)
+
+  cy.get('h3').should('have.text','ALL DEPARTMENTS')
+  cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click({force:true});
+  cy.get('[href="/account/membership/"]').click({force:true})
+  cy.xpath("//a[normalize-space()='shop now']").click({force:true})
+
+  cy.wait(4000)
+  cy.get('h3').should('have.text','ALL DEPARTMENTS')
+  cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click({force:true});
+  cy.get('[href="/account/membership/"]').click({force:true})
+  cy.xpath("//a[normalize-space()='start return']").contains('start return').click({force:true})
+  cy.get("button[type='button']").click()
+
+  cy.wait(4000)
+  cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click({force:true});
+  cy.get('[href="/account/membership/"]').click({force:true})
+  cy.xpath("//a[normalize-space()='get in touch']").click({force:true})
+  cy.get('h3').contains('contact Us')
+
+  cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click({force:true});
+  cy.get('[href="/account/membership/"]').click({force:true})
+
+
+
+
+
+
+})
+
+  it('Verify Wishlist', () => {
+    cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click();
+    cy.log('Clicking on login link');
+    cy.xpath("(//a[normalize-space()='Log in'])[1]").click();
+    cy.log('Filling in login form');
+    cy.get("input[placeholder='Email Address']").type('ve4df6765897ggsot82fdf226@sparkroi.com');
+    cy.get("input[placeholder='Password']").type('123456789');
+    cy.get("button[type='submit']").click();
+    cy.log('Checking Wishlist');
+    cy.wait(3000);
+  
+    cy.get('[href="/account/wishlist/"]').contains('Wishlist').click({ force: true });
+  
+    // Check if the wishlist is empty
+    cy.get('body').then(($body) => {
+      if ($body.find("h3:contains('Your Wishlist is empty')").length) {
+        // Wishlist is empty
+        cy.log('Your Wishlist is empty');
+        cy.xpath("(//h3[normalize-space()='Your Wishlist is empty'])[1]").should('have.text', 'Your Wishlist is empty').should('be.visible');
+  
+        // Click on "Add your favorite items"
+        cy.xpath("(//a[normalize-space()='Add your favorite items'])[1]").click({ force: true });
+        cy.wait(6000);
+  
+        // Add items to the wishlist
+        cy.xpath("/html[1]/body[1]/main[1]/main[1]/section[1]/div[3]/div[1]/ul[1]/li[1]/div[1]/div[1]/div[1]/div[1]/div[1]/span[1]/*[name()='svg'][1]").click({ force: true });
+        cy.wait(4000);
+        cy.xpath("/html[1]/body[1]/main[1]/main[1]/section[1]/div[3]/div[1]/ul[1]/li[3]/div[1]/div[1]/div[1]/div[1]/div[1]/span[1]/*[name()='svg'][1]").click({ force: true });
+        cy.wait(4000);
+        cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click({ force: true });
+        cy.get('[href="/account/wishlist/"]').contains('Wishlist').click({ force: true });
+  
+      } else {
+        // Wishlist is not empty
+        cy.log('You already have wishlisted products');
+      }
+    });
+
+
+  });
+  
+it('Verify Log Out', () => {
+    cy.xpath("//a[@aria-label='account-icon']//*[name()='svg']").click();
+    cy.log('Clicking on login link');
+    cy.xpath("(//a[normalize-space()='Log in'])[1]").click();
+    cy.log('Filling in login form');
+    cy.get("input[placeholder='Email Address']").type('ve4df6765897ggsot82fdf226@sparkroi.com');
+    cy.get("input[placeholder='Password']").type('123456789');
+    cy.get("button[type='submit']").click();
+    cy.log('Checking Log Out');
+    cy.wait(3000);
+
+    cy.get("button[type='submit']").click({force:true})
+})
 
 })
