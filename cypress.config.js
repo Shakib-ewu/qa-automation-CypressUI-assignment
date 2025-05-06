40a39ba5e7e5a5d3d824b4959e06b3e062f4b802
Shakib-ewu <hossainsakib58@gmail.com>
2025-05-06 13:24:54 +0600

    Upload all codes

--- cypress.config.js
+++ cypress.config.js
@@ -16,7 +16,7 @@ module.exports = defineConfig({
     openMode: 1,   // Retries when running via `cypress open`
   },
   e2e: {
-    baseUrl: "https://rubinoshoes.com/",
+    baseUrl: "https://craftcellars.ca/",
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
       // Add any custom Node event listeners here
---
+++ cypress/e2e/Xpath.cy.js
@@ -0,0 +1,14 @@
+describe('Verifying Homepage', () => {
+    beforeEach(() => {
+      cy.viewport(1920, 1080);
+      cy.intercept('/some-3rd-party-script.js*').as('externalScript');
+      cy.visit('https://craftcellars.ca/');
+      cy.wait(4000);
+    });
+
+    it('Verifying video detection', () => {
+        cy.scrollTo('center');
+        cy.xpath('//a[contains(text(), "Jack Daniel\'s Barrel Proof Rye - Craft Cellars Single Barrel Store Pick")]')
+          .should('exist').eq(0).click({force:true}); // Added assertion for better test validation
+    });
+});
