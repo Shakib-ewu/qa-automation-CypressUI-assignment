733f27e18b02b9e9f8c149ff2bd69cd7f9369677
Shakib-ewu <hossainsakib58@gmail.com>
2025-05-13 14:09:28 +0600

    New test cases added

--- cypress/e2e/Search.cy.js
+++ cypress/e2e/Search.cy.js
@@ -25,6 +25,70 @@ it('should return results for a valid keyword', () => {
 
 });
 
+it('should display no results message for an invalid search', () => {
+ cy.xpath("//input[contains(@id,'Search-In-Modal')]").eq(0).click(); // open search if hidden
+  cy.get('input[name="q"]').first().type('xyz123notfound{enter}');
+  cy.contains('No results found').should('be.visible');
+});
+
+it('should handle special characters gracefully', () => {
+ cy.xpath("//input[contains(@id,'Search-In-Modal')]").eq(0).click();
+  cy.get('input[name="q"]').first().type('!@#$%^&*(){enter}');
+  cy.contains('Search Results For !@#$%^&*()').should('be.visible');
+});
+
+it('should show suggestions while typing', () => {
+   cy.xpath("//input[contains(@id,'Search-In-Modal')]").eq(0).click();
+  cy.get('input[name="q"]').first().type('wine');
+  cy.get('.predictive_search_result_left').should('be.visible');
+});
+
+it('should return same results for upper/lowercase terms', () => {
+  cy.xpath("//input[contains(@id,'Search-In-Modal')]").eq(0).click();
+  cy.get('input[name="q"]').first().type('WHISKY{enter}');
+  cy.get('.product-grid').should('exist');
+
+  // Go back to the previous page
+  cy.go('back');
+
+  // Reopen the search modal again (because it's likely hidden now)
+  cy.xpath("//input[contains(@id,'Search-In-Modal')]").eq(0).click();
+
+  // Clear the input and type the lowercase version
+  cy.get('input[name="q"]').first().clear().type('whisky{enter}');
+  cy.get('.product-grid').should('exist');
+});
+
+it('should navigate to correct product page on result click', () => {
+  cy.xpath("//input[contains(@id,'Search-In-Modal')]").eq(0).click();
+  cy.get('input[name="q"]').first().type('scotch{enter}');
+  cy.get('.product-grid a:visible').first().click();
+  cy.url().should('include', '/products/');
+});
+
+it('should load more products when "Load More" is clicked', () => {
+  cy.xpath("//input[contains(@id,'Search-In-Modal')]").eq(0).click();
+
+  // Type a keyword and press enter
+  cy.get('input[name="q"]').first().type('whisky{enter}');
+
+  // Wait for initial products to load
+  cy.get('.product-grid .card__content').then(initialItems => {
+    const initialCount = initialItems.length;
+
+    // Click the "Load More" button
+    cy.contains('button', 'Load More')
+      .should('be.visible')
+      .click();
+
+    // Assert more products are loaded
+    cy.get('.product-grid .card__content')
+      .should('have.length.greaterThan', initialCount);
+  });
+});
+
+
+
 
 });
   
